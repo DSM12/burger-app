b35b3e05b3a944434356d84abd36bb443282097e
Danny Montano <dsmontano12@gmail.com>
2020-08-13 01:15:48 -0400

    deleted and recloned repo

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name) VALUES ("Morning Burger", false);
+INSERT INTO burgers (burger_name) VALUES ("Classic American Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salmon Burger", true);
