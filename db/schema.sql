374de88fdc4778c35d96f33e5ee35b0ab7449b16
Danny Montano <dsmontano12@gmail.com>
2020-08-15 10:07:45 -0400

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 id int NOT NULL AUTO_INCREMENT, 
 burger_name varchar(250) NOT NULL, 
-devoured BOOLEAN NOT NULL,
+devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+use burgers_db;
 INSERT INTO burgers (burger_name) VALUES ("Cheeseburger", false);
 INSERT INTO burgers (burger_name) VALUES ("Morning Burger", false);
 INSERT INTO burgers (burger_name) VALUES ("Classic American Burger", false);
